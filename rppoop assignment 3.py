12b366c48143fddf8a47924a729cc8bf020930bc
TejalJoshi116 <124522723+TejalJoshi116@users.noreply.github.com>
2023-05-26 11:15:36 +0530

    Update rppoop assignment 3.py

--- rppoop assignment 3.py
+++ rppoop assignment 3.py
@@ -1,82 +1,129 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
+import pandas as pd
+import openpyxl
+import os
 
 class BankAccount:
-    def __init__(self, name, account_number, account_type, balance):
+    def __init__(self, name, account_no, account_type, balance):
         self.name = name
-        self.account_number = account_number
+        self.account_no = account_no
         self.account_type = account_type
         self.balance = balance
-
-    def validate_account_number(self):
-        if len(self.account_number) != 10:
-            print("Invalid account number. Account number must be 10 digits long.")
-            return False
-        return True
-
+    
     def deposit(self, amount):
-        if amount < 10000:
-            print("Amount to be deposited must be more than 10000.")
-            return
-
         self.balance += amount
-        print("Amount deposited successfully.")
-
+        print("Amount deposited:", amount)
+        print("Updated balance:", self.balance)
+    
     def withdraw(self, amount):
-        if amount > self.balance:
-            print("Insufficient balance. Withdrawal failed.")
-            return
-
-        self.balance -= amount
-        print("Amount withdrawn successfully.")
-
-    def display_details(self):
-        print("Account Details:")
+        if self.balance >= amount:
+            self.balance -= amount
+            print("Amount withdrawn:", amount)
+            print("Updated balance:", self.balance)
+        else:
+            print("Insufficient balance!")
+    
+    def display_info(self):
         print("Name:", self.name)
-        print("Account Number:", self.account_number)
-        print("Account Type:", self.account_type)
+        print("Account number:", self.account_no)
+        print("Account type:", self.account_type)
         print("Balance:", self.balance)
+        
+# Function to write data to an Excel file
+def save_data(accounts):
+    data = {"Name": [], "Account Number": [], "Account Type": [], "Balance": []}
+    for account in accounts:
+        data["Name"].append(account.name)
+        data["Account Number"].append(account.account_no)
+        data["Account Type"].append(account.account_type)
+        data["Balance"].append(account.balance)
+    df = pd.DataFrame(data)
+    df.to_excel("ass3.xlsx", index=False)
+    print("Data saved to ass3.xlsx")
 
-# Creating a bank account object
-account = BankAccount("John Doe", "1234567890", "Savings", 5000)
+# Function to read data from an Excel file
+def load_data():
+    try:
+        df = pd.read_excel("ass3.xlsx")
+        accounts = []
+        for i, row in df.iterrows():
+            account = BankAccount(row["Name"], row["Account Number"], row["Account Type"], row["Balance"])
+            accounts.append(account)
+        print("Data loaded from ass3.xlsx")
+        return accounts
+    except FileNotFoundError:
+        print("File not found! Creating a new one.")
+        return []
+
+# Function to display the menu and get user input
+def display_menu():
+    print("\nMenu:")
+    print("1. Add account")
+    print("2. Deposit amount")
+    print("3. Withdraw amount")
+    print("4. Display account info")
+    print("5. Save data to file")
+    print("6. Load data from file")
+    print("7. Exit")
+    choice = int(input("Enter your choice: "))
+    return choice
+
+# Main program
+accounts = load_data()
 
 while True:
-    print("\n----- Bank Account Menu -----")
-    print("1. Deposit Amount")
-    print("2. Withdraw Amount")
-    print("3. Display Account Details")
-    print("0. Exit")
-
-    choice = input("Enter your choice (0-3): ")
-
-    if choice == "1":
-        amount = float(input("Enter the amount to deposit: "))
-        if account.validate_account_number():
-            account.deposit(amount)
-    elif choice == "2":
-        amount = float(input("Enter the amount to withdraw: "))
-        if account.validate_account_number():
-            account.withdraw(amount)
-    elif choice == "3":
-        account.display_details()
-    elif choice == "0":
-        print("Thank you for using the Bank Account System. Goodbye!")
+    choice = display_menu()
+    
+    if choice == 1:
+        name = input("Enter name: ")
+        account_no = input("Enter account number: ")
+        account_type = input("Enter account type: ")
+        balance = float(input("Enter balance: "))
+        account = BankAccount(name, account_no, account_type, balance)
+        accounts.append(account)
+        print("Account created successfully!")
+        
+    elif choice == 2:
+        account_no = input("Enter account number: ")
+        amount = float(input("Enter amount to deposit: "))
+        for account in accounts:
+            if account.account_no == account_no:
+                account.deposit(amount)
+                break
+        else:
+            print("Account not found!")
+            
+    elif choice == 3:
+        account_no = input("Enter account number: ")
+        amount = float(input("Enter amount to withdraw: "))
+        for account in accounts:
+            if account.account_no == account_no:
+                account.withdraw(amount)
+                break
+        else:
+            print("Account not found!")
+    
+    elif choice == 4:
+        account_no = input("Enter account number: ")
+        for account in accounts:
+            if account.account_no == account_no:
+                print("Name:", account.name)
+                print("Account number:", account.account_no)
+                print("Account type:", account.account_type)
+                print("Balance:", account.balance)
+                break
+        else:
+            print("Account not found!")
+    
+    elif choice == 5:
+        wb = openpyxl.load_workbook("ass3.xlsx")
+        ws = wb.active
+        for account in accounts:
+            ws.append([account.name, account.account_no, account.account_type, account.balance])
+        wb.save("ass3.xlsx")
+        print("Data saved to ass3.xlsx file!")
+        
+    elif choice == 6:
         break
+        
     else:
-        print("Invalid choice. Please try again.")
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+        print("Invalid choice! Please try again.") 
